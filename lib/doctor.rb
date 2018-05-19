0fb83b85eaca46d2895b4d19597e25b08edb77ea
Amar Chatman <amarjerome@gmail.com>
2018-05-19 20:28:10 +0000

    automatically backed up by learn

---
+++ lib/doctor.rb
@@ -0,0 +1,20 @@
+class Doctor
+
+  attr_accessor :name, :date
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(date,patient)
+    @date = date
+    patient = 
+
+end
